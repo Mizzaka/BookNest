48a8f3e6f68ee085987761fe6ef8ab5511c773f4
Missaka Rathnapriya <136617330+Mizzaka@users.noreply.github.com>
2025-01-03 22:11:29 +0530

    update home

---
+++ client/src/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import MainHome from "./components/mainhome";
+import Footer from "./components/Footer/Footer";
+
+
+const Home = () => {
+  
+
+  return (
+    <> 
+      <Navbar />
+      <MainHome />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
